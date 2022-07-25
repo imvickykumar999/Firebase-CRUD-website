96f756daa246126b3ab453bb2cd1175cfe9f8783
imvickykumar999 <imvickykumar999@gmail.com>
2022-07-26 00:36:17 +0530

    done

--- app.py
+++ app.py
@@ -8,14 +8,61 @@ def index():
 	return render_template('index.html')
 
 
-@app.route('/getalluser', methods = ['GET', 'POST'])
-def getalluser():
-	if(request.method == 'GET'):
-		from mydatabase import fire
-		path = 'apigee'
+@app.route('/getalluser')
+def get_all_user():
+	from mydatabase import fire
+	path = 'apigee'
 
-		json_data = fire.call(path)
-		return jsonify(json_data)
+	json_data = fire.call(path)
+	return jsonify(json_data)
+
+
+@app.route('/getuserbyid/<unique_ID>')
+def get_user_by_id(unique_ID):
+
+	from mydatabase import fire
+	path = f'apigee/{unique_ID}'
+
+	json_data = fire.call(path)
+	return jsonify(json_data)
+
+
+@app.route('/getuserbyid')
+def get_user_by_id_form():
+
+	from mydatabase import fire
+	path = 'apigee'
+
+	json_data = fire.call(path)
+	lst = list(json_data.keys())
+
+	return render_template('getuserbyid.html',
+							lst=lst,
+						)
+		
+
+@app.route('/postrow')
+def post_row():
+	from mydatabase import fire
+	import random
+
+	ran = random.randint(1000, 9999)
+	data = {
+		'unique_ID' : ran,
+		'first name' : f'first_name_{ran}',
+		'last name' : f'last_name_{ran}',
+		'age' : f'age_{ran}',
+	}
+
+	path = f'apigee/{ran}'
+	fire.send(path, data)
+	return render_template('posted.html', data=data)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html',
+					 e='Page not Found'), 404
 
 
 if __name__ == '__main__':
