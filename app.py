ff1a05184695194cce9e56a84ed6cdd4ec244f7a
Vicky Kumar <imvickykumar999@gmail.com>
2022-07-29 15:10:02 +0530

    ipynb file viewer

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 from flask import Flask, jsonify, request, render_template
+import ast, json, urllib.request as ur
 app = Flask(__name__)
 
 
@@ -133,6 +134,65 @@ def created_user():
 							submitted = True,
 							data=data
 							)
+
+
+@app.route("/ipynb")
+def ipynb():
+    return render_template("ipynb.html", infolist = [["Output"]])
+
+@app.route('/converted_ipynb', methods=['POST'])
+def convert_ipynb():
+
+    def ipynbinfo(info, file_name):
+        def call(file_name):
+
+            if 'http' == file_name[0:4]:
+                print('\nPlease WAIT, content is loading from URL...\n')
+                su = ur.urlopen(str(file_name)).read().decode('ascii')
+
+            elif '{' == file_name[0]:
+                su = file_name
+
+            elif '\\' or '/' in file_name:
+                su = open(file_name).read()
+
+            try:
+                y = json.loads(str(su))
+            except:
+                y = su
+            return ast.literal_eval(str(y))
+
+        def recdict(d):
+            try:
+                box.append(d[info])
+            except Exception as e:
+                pass
+
+            for i in list(d.values()):
+                if type(i) == list:
+                    for j in i:
+
+                        if type(j) == dict:
+                            recdict(j)
+
+                if type(i) == dict:
+                    recdict(i)
+            return box
+        return recdict(call(file_name))
+
+    n, box = 130, []
+    file_name = request.form['ipynb']
+
+    if file_name == '':
+        file_name = 'https://raw.githubusercontent.com/imvickykumar999/vixtor/master/vixtor.ipynb'
+
+    infolist = ipynbinfo('source', file_name)
+    # for i in infolist:
+    #     for j in i:
+    #         print(j)
+    #     print('='*n, end='\n\n')
+
+    return render_template('ipynb.html', infolist = infolist, range = range(40))
 
 
 @app.errorhandler(404)
